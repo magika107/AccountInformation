e7bf9d9c8beff3951f4880371aa0775ed530f594
Mehdi.ghasemi <mehdighasemi10710@gmail.com>
2025-06-06 19:59:59 +0330

    edit Login and Id

--- src/main/java/mft/model/repository/ConnectionProvider.java
+++ src/main/java/mft/model/repository/ConnectionProvider.java
@@ -1,27 +1,40 @@
 package mft.model.repository;
 
 import lombok.Getter;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionProvider {
     @Getter
     private static ConnectionProvider connectionProvider = new ConnectionProvider();
+    private static BasicDataSource basicDataSource = new BasicDataSource();
+
 
     private ConnectionProvider() {
     }
 
     public Connection getConnection() throws SQLException {
-        Connection connection = DriverManager.getConnection(
-                "jdbc:oracle:thin:@localhost:1521:xe",
-                "java",
-                "java123"
+        // basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        // basicDataSource.setUrl("jdbc:mysql://localhost:3306/meta_laptop");
 
-        );
-      //  connection.setAutoCommit(false);
-        return connection;
+
+        basicDataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+        basicDataSource.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+        basicDataSource.setUsername("java");
+        basicDataSource.setPassword("java123");
+        basicDataSource.setMinIdle(5);
+        return basicDataSource.getConnection();
+
+
+//        Connection connection = DriverManager.getConnection(
+//                "jdbc:oracle:thin:@localhost:1521:xe",
+//                "java",
+//                "java123"
+//        );
+        //  connection.setAutoCommit(false);
+        //return connection;
     }
 }
 
